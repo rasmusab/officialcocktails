3685681f07756840f3a705edff88ba6da4f7bf80
Rasmus Bååth <rasmus.baath@gmail.com>
2023-03-19 21:01:39 +0100

    Added comments and minor tweaks to the scripts

--- 01_regenerate-assets.R
+++ 01_regenerate-assets.R
@@ -1,3 +1,13 @@
+# This script will create a cocktail-descriptions.yaml file if it doesn't already exist.
+# This is a yaml file that contains all the drink info scraped from wikipedia
+# (from iba-cocktails-wiki.json) plus NULL fields for all the things we want to 
+# fill in, like the image, fun facts, etc. This script will then start filling in all 
+# the nulls. As the ChatGPT "hangs" regularly, I needed to rerun this script many times
+# until cocktail-descriptions.yaml was completely filled in.
+#
+# If a generated description or images is wonky, just delete it and rerun this script
+# and it will be regenerated.
+
 library(tidyverse)
 library(yaml)
 library(jsonlite)
@@ -33,7 +43,9 @@ dir.create("cocktail-images", showWarnings = FALSE)
 
 # Calls the ChatGPT API with the given prompt and returns the answer
 generate_text <- function(prompt) {
-  response <- POST(
+  response <- RETRY(
+    "POST",
+    times = 3,
     url = "https://api.openai.com/v1/chat/completions", 
     add_headers(Authorization = paste("Bearer", openai_api_key)),
     content_type_json(),
@@ -44,12 +56,15 @@ generate_text <- function(prompt) {
         role = "user", 
         content = prompt
       ))
-    )
+    ),
+    timeout(seconds = 30)
   )
   str_trim(content(response)$choices[[1]]$message$content)
 }
 
-
+# Calls a Stable Diffusion API to get the an image. Why not DALL-E?
+# Well, some drinks have "shocking" names (like Corpse Reviver), and
+# DALL-E content filter refuses to make any images of those drinks.
 generate_image <- function(prompt) {
     generate_response <- POST(
     url = "https://xarrreg662.execute-api.us-east-1.amazonaws.com/sdAddEle", 
@@ -74,22 +89,6 @@ generate_image <- function(prompt) {
   image_url
 }
 
-generate_image_dalle <- function(prompt) {
-  response <- POST(
-    url = "https://api.openai.com/v1/images/generations", 
-    add_headers(Authorization = paste("Bearer", openai_api_key)),
-    content_type_json(),
-    encode = "json",
-    body = list(
-      prompt = prompt, 
-      n = 1,
-      size = "512x512", # "1024x1024"
-      response_format = "url"
-      )
-    )
-  print(response)
-  content(response)$data[[1]]$url
-}
 
 generate_and_save_image <- function(prompt, path) {
   image_url <- generate_image(prompt)
@@ -97,6 +96,9 @@ generate_and_save_image <- function(prompt, path) {
   path
 }
 
+# A function that takes a prompt template and generates another function which 
+# takes a cocktail, as from cocktail_descriptions, completes the template and returns
+# a ChatGPT answer.
 gen_text_func <- function(...) {
   function(cocktail) {
     prompt <- glue(..., .envir = cocktail, .sep = "\n")
@@ -105,10 +107,13 @@ gen_text_func <- function(...) {
   }
 }
 
+# A list of generator functions that can generate text/images for the corresponding
+# field, if missing.
 generators <- list( 
   image_path = \(cocktail) {
     path = file.path("cocktail-images",  paste0(cocktail$base_fname, ".jpeg"))
     prompt = glue("A well-lit photo of a {name} drink served in a {standard_drinkware} standing on a bartop, professional food photography, 15mm", .envir = cocktail)
+    cat(glue("Generating image for: {prompt}"), "\n")
     generate_and_save_image(prompt, path)
     path
   },
@@ -119,23 +124,13 @@ generators <- list(
     "Describe the steps to make a {name} drink as a markdown formated list. Don't repeat the list of ingredients."
   ),
   tips = gen_text_func("Give a couple of tips for how to make the perfect {name} drink. Format the response as markdown. Start directly with the tips and skip the headline."),
-  history = gen_text_func("Describe the origin and history of the drink {name}. Format the response as markdown."),
   fun_facts = gen_text_func("List five fun facts about the drink {name}. Format the response as markdown."),
   alcohol_free_alternative = gen_text_func("Describe an alcohol free alternative to the drink {name}")
 )
 
-
-# completed_cocktail_descriptions <- map(cocktail_descriptions, \(cocktail) {
-#   if(! is.null(cocktail$image_path) && ! file.exists(cocktail$image_path)) {
-#     cocktail["image_path"] <- list(NULL)
-#   }
-#   items_to_fill <- cocktail |>  keep(is.null) |> names()
-#   for(item in items_to_fill) {
-#     cocktail[item] <- generators[[item]](cocktail)
-#   }
-#   cocktail
-# })
-
+# Looping through the cocktail description and filling in all the blanks.
+# As both the text and image generating APIs errors out now and then
+# this loop saves every single response directly to cocktail-descriptions.yaml
 for(cocktail_i in seq_along(cocktail_descriptions)) {
   cocktail <- cocktail_descriptions[[cocktail_i]]
   if(! is.null(cocktail$image_path) && ! file.exists(cocktail$image_path)) {
@@ -148,7 +143,3 @@ for(cocktail_i in seq_along(cocktail_descriptions)) {
     write_yaml(cocktail_descriptions, "cocktail-descriptions.yaml") 
   }
 }
-
-
-
-
